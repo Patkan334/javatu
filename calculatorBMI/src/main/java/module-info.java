9ec8c8bcda0952c4f743daf61380255485da7cca
Patkan334 <115421144+Patkan334@users.noreply.github.com>
2022-12-20 11:46:07 +0200

    Delete calculatorBMI directory

--- calculatorBMI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.bmicalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bmicalculator to javafx.fxml;
-    exports com.example.bmicalculator;
-}
