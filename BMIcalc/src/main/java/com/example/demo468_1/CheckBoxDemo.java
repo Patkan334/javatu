51818f21f6255541ad716f1f5a3bf762e4d0439e
Patkan334 <115421144+Patkan334@users.noreply.github.com>
2022-12-22 17:54:20 +0200

    Add files via upload

---
+++ BMIcalc/src/main/java/com/example/demo468_1/CheckBoxDemo.java
@@ -0,0 +1,24 @@
+package com.example.demo468_1;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+/*
+public class CheckBoxDemo extends ButtonDemon {
+
+    @Override // Override the getPane() method in the super class
+protected BorderPane getPane() {
+        BorderPane pane = super.getPane();
+        Font fontBoldItalic = Font.font("Times New Roman", FontWeight.BOLD,
+                FontPosture.ITALIC, 20);
+
+    }
+}
+
+ */
